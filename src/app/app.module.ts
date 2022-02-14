a70e4d49f6bcdeb20f47f202229e1adfa1518968
subinoy paik <subinoy.swadesh@gmail.com>
2022-02-14 12:54:25 +0530

    calender  page all  changes has done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RemovehtmltagsPipe } from './removehtmltags.pipe';
+
 //import { Storage} from '@ionic/storage';
 
 @NgModule({
--- src/app/calender/calender.page.ts
+++ src/app/calender/calender.page.ts
@@ -10,15 +10,41 @@ import { ModalController } from '@ionic/angular';
 })
 export class CalenderPage implements OnInit {
   @ViewChild(IonDatetime, { static: true }) datetime: IonDatetime;
-  dateValue = '';
-  dateValue2 = '';
+ 
 
-  constructor(public modalCtrl: ModalController) {}
+  constructor(public modalCtrl: ModalController) {console.log(this.myDate)}
 
-  date = new Date();
+  // time// array//
+   timesidule=[
+    {
+      time:"10.00",
+    },
+    {
+      time:"11.00",
+    }
+  ]
+  
+//past date disable fiexd//
+date = new Date();
+  minDate =  new Date(this. date.getTime() - 
+              this.date.getTimezoneOffset()*60000).toISOString();
+
+  // time local fiexd//
+  
   myDate: String = new Date(this.date.getTime() - 
                    this.date.getTimezoneOffset()*60000).toISOString();
-  
-  ngOnInit() {
+                              
+
+  // data get//
+  datechange(value){
+    console.log(value)
+
   }
+ //time//
+ timedata(){
+  console.log("hi")
+ }
+
+  ngOnInit() {}
+  
 }
